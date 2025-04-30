088127052e68b9209d2ec95088630c888236fa05
Teio <2894666593@qq.com>
2025-04-30 09:53:27 +0800

    冗余代码优化 4.30/9.53

--- test.py
+++
@@ -1,9 +0,0 @@
-from paddleocr import PaddleOCR
-
-# 初始化PaddleOCR
-ocr = PaddleOCR(use_angle_cls=True, lang='ch')
-print("PaddleOCR初始化成功")
-
-# 测试识别一张简单图片
-result = ocr.ocr('test.png')  # 替换为你的任意图片
-print("识别结果:", result)
